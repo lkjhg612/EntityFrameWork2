fceb6012b5880e3ed5568a101d596422c9624adc
lkjhg612 <56185151+lkjhg612@users.noreply.github.com>
2021-11-17 22:02:56 +0700

    Add project files.

---
+++ EntityFrameWork2/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameWork2.Controllers
+{
+    public class HomeController : Controller
+    {
+        QuanCaPheEntities pheEntities = new QuanCaPheEntities();
+        public ActionResult Index()
+        {
+            var cf = pheEntities.MatHangs.ToList(); 
+            return View(cf);
+        }
+    }
+}
---
+++ EntityFrameWork2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntityFrameWork2.MvcApplication" Language="C#" %>
